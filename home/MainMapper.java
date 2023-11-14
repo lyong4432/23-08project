0479f8cf5133b837813b19ce361f698f01aad99a
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-11-14 17:45:04 +0900

    Create MainMapper.java

---
+++ home/MainMapper.java
@@ -0,0 +1,14 @@
+package kr.or.project.mapper;
+
+import java.util.List;
+
+import kr.or.project.annotation.MariaDbMapper;
+import kr.or.project.main.model.NoticeInfo;
+
+@MariaDbMapper
+public interface MainMapper {
+	
+	public List<NoticeInfo> selectMainInfo(NoticeInfo noticeInfo) throws Exception;
+	
+	public int deleteMainInfo(NoticeInfo noticeInfo) throws Exception;
+}
