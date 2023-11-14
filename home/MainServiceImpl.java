1da32b20da9e8f11498216ac2b13b5bf95399329
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-11-14 17:46:09 +0900

    Create MainServiceImpl.java

---
+++ home/MainServiceImpl.java
@@ -0,0 +1,38 @@
+package kr.or.project.main.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.or.project.main.model.NoticeInfo;
+import kr.or.project.main.service.MainService;
+import kr.or.project.mapper.MainMapper;
+
+@Service
+public class MainServiceImpl implements MainService {
+	
+	@Autowired
+	private MainMapper mapper;
+	
+	@Override
+	public List<NoticeInfo> selectMainInfo(NoticeInfo noticeInfo) {
+		try {
+			return mapper.selectMainInfo(noticeInfo);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	@Override
+	public int deleteMainInfo(NoticeInfo noticeInfo) {
+		try {
+			return mapper.deleteMainInfo(noticeInfo);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+}
